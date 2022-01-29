b6e2b3c6fca43d1774e890d960a347a5fe0e46bc
Bernatto <braianbernatto@hotmail.com>
2022-01-29 00:38:36 -0300

    factura query updated

--- API/models/Factura.js
+++ API/models/Factura.js
@@ -8,8 +8,8 @@ Factura.allFacturasByContrato = async function (licitacionID, contratoNro) {
   return new Promise(async (resolve, reject) => {
     try {
       let resultado =
-        await pool.query(`select f.factura_nro, f.factura_fecha, f.factura_monto, f.factura_timbrado, s.str_nro, s.str_year, s.str_fecha, sd.str_monto from factura f left join str_detalle sd natural join str s 
-     on f.factura_nro = sd.factura_nro and f.factura_timbrado = sd.factura_timbrado inner join contrato con on f.contrato_nro = con.contrato_nro where con.licitacion_id = ${licitacionID} and con.contrato_nro = ${contratoNro} order by factura_fecha desc, factura_nro desc`)
+        await pool.query(`select f.factura_nro, f.factura_fecha, f.factura_monto, f.factura_timbrado, s.str_nro, s.str_year,
+        s.str_fecha, m.moneda_simbolo, sd.str_monto from factura f left join str_detalle sd natural join str s natural join moneda m on f.factura_nro = sd.factura_nro and f.factura_timbrado = sd.factura_timbrado inner join contrato con on f.contrato_nro = con.contrato_nro where con.licitacion_id = ${licitacionID} and con.contrato_nro = ${contratoNro} order by factura_fecha desc, factura_nro desc`)
 
       // resultado.length ? resolve(resultado) : reject()
 
